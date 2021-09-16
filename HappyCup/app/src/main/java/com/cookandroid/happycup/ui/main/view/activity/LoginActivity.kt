ccf64158d7567b3c723579f5318835eafd9eabb9
yuyw0712 <yuyw0712@naver.com>
2021-09-16 21:37:07 +0900

    Gradle 구성 및 기본 작업

---
+++ HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.happycup.ui.main.view.activity
+
+import com.cookandroid.happycup.R
+import com.cookandroid.happycup.databinding.ActivityLoginBinding
+import com.cookandroid.happycup.ui.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun init() {
+        super.init()
+
+    }
+}
---
+++ HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/activity/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.cookandroid.happycup.ui.main.view.activity
+
+class MainActivity {
+}
