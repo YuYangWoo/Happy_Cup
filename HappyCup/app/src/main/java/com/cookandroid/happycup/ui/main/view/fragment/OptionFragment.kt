ec796e3f960b733789d686651b130281e87da4d6
yuyw0712 <yuyw0712@naver.com>
2021-09-18 23:15:24 +0900

    Logout setup

--- HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/fragment/OptionFragment.kt
+++ HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/fragment/OptionFragment.kt
@@ -1,8 +1,25 @@
 package com.cookandroid.happycup.ui.main.view.fragment
 
+import android.content.Intent
 import com.cookandroid.happycup.ui.base.BaseFragment
 import com.cookandroid.happycup.R
+import com.cookandroid.happycup.data.singleton.MySharedPreferences
 import com.cookandroid.happycup.databinding.FragmentOptionBinding
+import com.cookandroid.happycup.ui.main.view.activity.LoginActivity
 
 class OptionFragment : BaseFragment<FragmentOptionBinding>(R.layout.fragment_option) {
+
+    override fun init() {
+        super.init()
+        logout()
+    }
+
+    private fun logout() {
+        binding.btnLogout.setOnClickListener {
+            MySharedPreferences.clearUser(requireContext())
+            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            requireActivity().finish()
+        }
+    }
+
 }
