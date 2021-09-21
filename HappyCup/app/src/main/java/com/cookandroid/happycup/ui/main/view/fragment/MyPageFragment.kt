65b5747544d43375ba02a14d8b3db93e3addfa0f
yuyw0712 <yuyw0712@naver.com>
2021-09-22 01:59:02 +0900

    MyPage Button Implement

--- HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/fragment/MyPageFragment.kt
+++ HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/fragment/MyPageFragment.kt
@@ -1,12 +1,46 @@
 package com.cookandroid.happycup.ui.main.view.fragment
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.cookandroid.happycup.ui.base.BaseFragment
 import com.cookandroid.happycup.R
+import com.cookandroid.happycup.data.singleton.MySharedPreferences
 import com.cookandroid.happycup.databinding.FragmentMypageBinding
+import com.cookandroid.happycup.ui.main.view.activity.LoginActivity
+import org.koin.android.ext.android.bind
 
 class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
 
     override fun init() {
         super.init()
+        logout()
+        btnSetOnClick()
     }
+
+    private fun btnSetOnClick() {
+        binding.btnAlram.setOnClickListener {
+        }
+
+        binding.btnLeftArrow.setOnClickListener {
+            findNavController().navigate(MyPageFragmentDirections.actionMyPageFragmentToMainFragment())
+        }
+
+        binding.btnSetting.setOnClickListener {
+            findNavController().navigate(MyPageFragmentDirections.actionMyPageFragmentToOptionFragment())
+        }
+    }
+
+    private fun logout() {
+        binding.btnLogout.setOnClickListener {
+            MySharedPreferences.clearUser(requireContext())
+            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            requireActivity().finish()
+        }
+    }
+
 }
