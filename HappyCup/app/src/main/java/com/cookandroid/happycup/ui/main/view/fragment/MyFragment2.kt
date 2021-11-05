46c4495352e0ed69b9662cd7c6d82959db2b6596
박성준 <63576379+tjdwns4537@users.noreply.github.com>
2021-11-05 19:22:34 +0900

    쇼핑몰, 검색 fragment 추가

---
+++ HappyCup/app/src/main/java/com/cookandroid/happycup/ui/main/view/fragment/MyFragment2.kt
@@ -0,0 +1,18 @@
+package com.cookandroid.happycup.ui.main.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cookandroid.happycup.R
+
+class MyFragment2 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my2, container, false)
+    }
+}
